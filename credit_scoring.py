6482e22f8482711b5947c65c7b2b17d5fd9cb45a
cckmwong-data <cckmwong.data@gmail.com>
2025-10-02 15:04:21 +0100

    Add files via upload

--- credit_scoring.py
+++ credit_scoring.py
@@ -71,7 +71,7 @@ if st.button("Predict"):
 
     st.subheader("🔎 What drove the decision?")
     st.write("Blue bars push the prediction higher towards repayment, whereas red bars push the prediction lower towards default. The contributions add up to the final prediction f(x).")
-    st.write("The gray value on the left (E[f(X)]) is the model’s average prediction (baseline).")
+    st.write("The gray value on the left (E[f(X)]) is the model’s average prediction.")
     st.write("Remarks: DTI (debt-to-income ratio) is to assess a borrower’s ability to manage monthly payments and repay debts. DTI = Total Monthly Debt Payments/ Monthly Income")
 
     # Create a waterfall plot and render in Streamlit
