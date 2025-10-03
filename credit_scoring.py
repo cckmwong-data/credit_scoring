0faacaa66850fb2a4a184bd90196aa662849da28
cckmwong-data <cckmwong.data@gmail.com>
2025-10-03 11:09:33 +0100

    Add files via upload

--- credit_scoring.py
+++ credit_scoring.py
@@ -59,8 +59,8 @@ if st.button("Predict"):
     prob_default = model.predict_proba(input_data_scaled)[0][1]   # class 1 = default
     prob_repay   = model.predict_proba(input_data_scaled)[0][0]   # class 0 = non-default
 
-    apply_result = "APPROVED ✅" if prob_default <= 0.35 else "DECLINED ❌"
-    apply_reason = "Since the repayment probability is above this thresold, the loan application is " if prob_default <= 0.35 else "Since the repayment probability falls below this thresold, the loan application is "
+    apply_result = "APPROVED. ✅" if prob_default <= 0.35 else "DECLINED. ❌"
+    apply_reason = "As the repayment probability is above this thresold, the loan application is " if prob_default <= 0.35 else "As the repayment probability falls below this thresold, the loan application is "
 
     st.subheader("Prediction Result")
     st.write(f"The lender adopts a prudent approach by setting a lower decision threshold of 35% compared to the standard 50%. {apply_reason}**{apply_result}**")
